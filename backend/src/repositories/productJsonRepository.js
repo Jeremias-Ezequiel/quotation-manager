475cc7ddab2db3ba9df18837e4d1e968b47f33d0
Omonte Jeremias <jeremias150817@gmail.com>
2026-04-01 20:08:55 -0300

    feat: added productJsonRepository that interact with the json file to extract the data

---
+++ backend/src/repositories/productJsonRepository.js
@@ -0,0 +1,16 @@
+import IProductInterface from "../interfaces/IProductInterface.js";
+import fs from "node:fs";
+import path from "node:path";
+
+class ProductJsonRepository extends IProductInterface {
+  async getAll() {
+    const jsonPath = path.join(process.cwd(), "/src/data/products.json");
+    const data = fs.readFileSync(jsonPath, "utf-8");
+    return JSON.parse(data);
+  }
+  async getById(id) {}
+  async create(data) {}
+  async update() {}
+}
+
+export default ProductJsonRepository;
