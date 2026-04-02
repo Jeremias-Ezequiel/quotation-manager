e4cff7434062e3ea9a4a1ad3cf86145769964fe5
Omonte Jeremias <jeremias150817@gmail.com>
2026-04-02 11:38:29 -0300

    feat: added cors to allow the communication in differents origins

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,23 @@
 import express from "express";
 import "dotenv/config";
+import cors from "cors";
+
+// Routes
+import notFound from "./src/middlewares/NotFound.js";
+import productRoute from "./src/routes/product.routes.js";
 
 const app = express();
 
-app.use((req, res) => {
-  res.json("Hello world");
-});
+app.use(cors());
+
+app.use(express.json());
 
 const PORT = process.env.PORT || 3001;
+
+app.use("/api", productRoute);
+
+// Show a message when the endpoint is incorrect
+app.use(notFound);
 
 app.listen(PORT, () => {
   console.log(`Server on: http://localhost:${PORT}`);
