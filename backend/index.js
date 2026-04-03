d2aeb7fce165071e31f5b3bdb3ac59a0f9d4a549
Omonte Jeremias <jeremias150817@gmail.com>
2026-04-02 23:09:46 -0300

    feat: add cors

--- backend/index.js
+++ backend/index.js
@@ -5,6 +5,7 @@ import cors from "cors";
 // Routes
 import notFound from "./src/middlewares/NotFound.js";
 import productRoute from "./src/routes/product.routes.js";
+import exchangeRoute from "./src/routes/exchange-rates.routes.js";
 
 const app = express();
 
@@ -15,6 +16,7 @@ app.use(express.json());
 const PORT = process.env.PORT || 3001;
 
 app.use("/api", productRoute);
+app.use("/api", exchangeRoute);
 
 // Show a message when the endpoint is incorrect
 app.use(notFound);
