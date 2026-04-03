091fde7a904b2c6c2513c441d745a239bca6ce06
Omonte Jeremias <jeremias150817@gmail.com>
2026-04-02 23:06:39 -0300

    update: delete the getByCategory functio and add filter into the getAll funcion to filter the data with the query parameters from the url

--- backend/src/services/productService.js
+++ backend/src/services/productService.js
@@ -8,12 +8,34 @@ class ProductService {
 
   async create() {}
 
-  async getAll() {
-    const products = await this.#repository.getAll();
+  async getAll(queries) {
+    const { category, sort } = queries;
+
+    let products = await this.#repository.getAll();
+
+    // ByCateories
+    if (category) {
+      products = products.filter((p) => p.category === category);
+    }
+
+    // Order By
+    if (sort) {
+      switch (sort) {
+        case "name":
+          products = products.sort((a, b) => a.name.localeCompare(b.name));
+          break;
+        case "asc":
+          products = products.sort((a, b) => a.price_usd - b.price_usd);
+          break;
+        case "desc":
+          products = products.sort((a, b) => b.price_usd - a.price_usd);
+          break;
+          deafult: break;
+      }
+    }
+
     return products;
   }
-
-  async getById(id) {}
 }
 
 export default ProductService;
