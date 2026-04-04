9a5e7eb0cb07a33e32098116407c3f3cde9bdb3f
Omonte Jeremias <jeremias150817@gmail.com>
2026-04-04 18:01:44 -0300

    feat: added render quote list

--- frontend/renders.js
+++ frontend/renders.js
@@ -1,4 +1,4 @@
-import { formatPrice } from "./utils/utils.js";
+import { formatPrice, calculateIva } from "./utils/utils.js";
 
 export function renderProductList(products, containerId, { data: priceArs }) {
   const container = document.getElementById(containerId);
@@ -29,7 +29,7 @@ export function renderProductList(products, containerId, { data: priceArs }) {
                         </ul>
                         <div class="card-body d-flex gap-3">
                           <button href="#" data-sku='${sku}' class="btn btn-primary btn-show-details">SHOW DETAILS</button>
-                          <button href="#" class="btn btn-primary">QUOTE PRICE</button>
+                          <button href="#" data-sku='${sku}' class="btn btn-primary btn-add-quote">QUOTE PRICE</button>
                           </div>
                         </div>`;
     container.appendChild(div);
@@ -54,9 +54,8 @@ export function renderPriceUsdToArs(value, containerId) {
   container.appendChild(div);
 }
 
-export function showModal({ data, priceArs }) {
-  const [product] = data;
-  const { name, price_usd } = product;
+export function showModal({ data }) {
+  const { name, price_usd, priceArs } = data;
 
   document.getElementById("modal-product-title").innerHTML = name;
   document.getElementById("modal-product-usd").innerHTML =
@@ -72,4 +71,82 @@ export function showModal({ data, priceArs }) {
   const modal = document.getElementById("detailProductModal");
   const instBootstrap = new bootstrap.Modal(modal);
   instBootstrap.show();
+}
+
+export function renderQuoteList(products) {
+  const container = document.getElementById("quotation-list");
+  container.innerHTML = "";
+
+  let totalQuotationArs = 0;
+  let totalQuotationUsd = 0;
+
+  products.forEach((p) => {
+    const { name, brand, price_usd, priceArs } = p;
+    const totalPriceArs = priceArs * price_usd;
+
+    totalQuotationArs += totalPriceArs;
+    totalQuotationUsd += price_usd;
+
+    const formatedTotalPriceARS = formatPrice(
+      price_usd * priceArs,
+      "es-AR",
+      "ARS",
+    );
+
+    const formatedUsd = formatPrice(price_usd);
+    const li = document.createElement("LI");
+    li.classList.add(
+      "d-flex",
+      "gap-3",
+      "border-bottom",
+      "border-5",
+      "border-primary",
+    );
+    li.innerHTML = `<h4>Name: ${name}</h4>
+                    <div class='d-flex flex-column'> 
+                      <p>USD: ${formatedUsd}</p>
+                      <p>ARS: ${formatedTotalPriceARS}</p>
+                    </div>
+    `;
+    container.appendChild(li);
+  });
+
+  const totalArsWithTaxes = calculateIva(totalQuotationArs);
+
+  totalArsWithTaxes.total = formatPrice(
+    totalArsWithTaxes.total,
+    "es-AR",
+    "ARS",
+  );
+
+  totalArsWithTaxes.taxe = formatPrice(totalArsWithTaxes.taxe, "es-AR", "ARS");
+
+  const formatedTotalQuotationArs = formatPrice(
+    totalQuotationArs,
+    "es-AR",
+    "ARS",
+  );
+  const formatedTotalQuotationUsd = formatPrice(totalQuotationUsd);
+
+  const div = document.createElement("DIV");
+  div.classList.add(
+    "fw-bold",
+    "fs-5",
+    "bg-secondary-subtle",
+    "p-3",
+    "rounded",
+    "my-3",
+  );
+  div.innerHTML = `<p>Subtotal USD: ${formatedTotalQuotationUsd}</p>
+                  <p>Subtotal ARS: ${formatedTotalQuotationArs}</p>
+                  <p>IVA (21%) ARS: ${totalArsWithTaxes.taxe}</p>
+                  <p>Final Total: ${totalArsWithTaxes.total}</p>
+  `;
+  container.appendChild(div);
+
+  const delButton = document.createElement("BUTTON");
+  delButton.classList.add("btn", "btn-danger");
+  delButton.id = "delQuoteList";
+  delButton.innerHTML = "Clear List";
+  container.append(delButton);
 }
