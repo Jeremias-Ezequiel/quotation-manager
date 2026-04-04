408e1498da7eee6a70b904f1e3879cb7dfa3c2d8
Omonte Jeremias <jeremias150817@gmail.com>
2026-04-03 22:56:53 -0300

    udpate: the filter logic in the productService was moved to productJsonRepositor because, the service does not have the responsibilty to do it

--- backend/src/repositories/productJsonRepository.js
+++ backend/src/repositories/productJsonRepository.js
@@ -3,10 +3,42 @@ import fs from "node:fs";
 import path from "node:path";
 
 class ProductJsonRepository extends IProductInterface {
-  async getAll() {
+  async getAll(queries = {}) {
     const jsonPath = path.join(process.cwd(), "/src/data/products.json");
     const data = fs.readFileSync(jsonPath, "utf-8");
-    return JSON.parse(data);
+    let products = JSON.parse(data);
+
+    const { category, sort, search } = queries;
+    console.log(category);
+    // By categories
+    if (category && category !== "all") {
+      products = products.filter((p) => p.category === category);
+    }
+
+    // Order By
+    if (sort) {
+      switch (sort) {
+        case "name":
+          products = products.sort((a, b) => a.name.localeCompare(b.name));
+          break;
+        case "asc":
+          products = products.sort((a, b) => a.price_usd - b.price_usd);
+          break;
+        case "desc":
+          products = products.sort((a, b) => b.price_usd - a.price_usd);
+          break;
+        default:
+          break;
+      }
+    }
+
+    if (search) {
+      products = products.filter((p) =>
+        p.name.toLowerCase().includes(search.toLowerCase()),
+      );
+    }
+
+    return products;
   }
 
   async getById(id) {}
--- backend/src/services/productService.js
+++ backend/src/services/productService.js
@@ -9,46 +9,21 @@ class ProductService {
   async create() {}
 
   async getAll(queries) {
-    const { category, sort, search } = queries;
+    let products = await this.#repository.getAll(queries);
 
-    let products = await this.#repository.getAll();
-
-    // ByCategories
-    if (category && category !== "all") {
-      products = products.filter((p) => p.category === category);
-      if (!products.length) {
-        throw new Error(`The category ${category} is not a valid category.`);
-      }
-    }
-
-    // Order By
-    if (sort) {
-      switch (sort) {
-        case "name":
-          products = products.sort((a, b) => a.name.localeCompare(b.name));
-          break;
-        case "asc":
-          products = products.sort((a, b) => a.price_usd - b.price_usd);
-          break;
-        case "desc":
-          products = products.sort((a, b) => b.price_usd - a.price_usd);
-          break;
-        default:
-          break;
-      }
-    }
-
-    if (search) {
-      products = products.filter((p) =>
-        p.name.toLowerCase().includes(search.toLowerCase()),
+    if (queries.category && !products.length) {
+      const err = new Error(
+        `The category ${queries.category} is not a valid category`,
       );
+      err.status = 400;
+      throw err;
     }
 
     return products;
   }
 
   async getProductBySku({ sku }) {
-    const regexPattern = /^[A-Z0-9]{3,}-[A-Z0-9]{3,}-[A-Z0-9]{3,}$/;
+    const regexPattern = /^([A-Z0-9]{2,}-){2}[A-Z0-9]{2,}(-[A-Z0-9]{2,})?$/;
 
     if (!regexPattern.test(sku)) {
       const err = new Error("Invalid SKU: xxx-xxx-xxx");
